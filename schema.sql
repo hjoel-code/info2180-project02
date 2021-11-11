98093e872f16ae52c6582adc4820e9ad164450b6
ezzy-web <90319401+ezzy-web@users.noreply.github.com>
2021-11-11 17:28:42 -0500

    Updated schema.sql

--- schema.sql
+++ schema.sql
@@ -12,10 +12,11 @@ USE bugme;
 -- Construction of the 'users' table
 -------------------------------------
 
-DROP TABLE IF EXISTS 'users';
+DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE 'users' (
-    'id' int(11) NOT NULL auto_increment,
-    'firstname' char(25) NOT NULL default '',
-    'lastname' char(25) NOT NULL default '',
-)
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL auto_increment,
+    `firstname` char(25) NOT NULL default '',
+    `lastname` char(25) NOT NULL default '',
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4080 DEFAULT CHARSET=utf8mb4;
