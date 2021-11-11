9de6258dda5425082958c068666f6c2ca93d805f
ezzy-web <90319401+ezzy-web@users.noreply.github.com>
2021-11-11 17:15:15 -0500

    Just to get us started

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+
 function ajax_methods(method, addr, data) {
     var response = {
         data: null,
@@ -19,10 +22,28 @@ function ajax_methods(method, addr, data) {
         .fail( error => {
             response.error = error
         })
-        
+
     } else {
         response.error = "INCOMPATIBLE REQUEST MEHTOD"
     }
 
     return response
 }
+
+
+function reload_page(page_title, content) {
+    document.title = "BugMe | " + page_title;
+    $('#page-content').html(content);
+}
+
+
+
+
+
+
+
+
+});
+
+
+
