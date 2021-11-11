90b65b056c262b9ed9c2a16e2ee8e6ded79453f9
ezzy-web <90319401+ezzy-web@users.noreply.github.com>
2021-11-11 18:48:26 -0500

    Alert Implementation

--- app.js
+++ app.js
@@ -38,15 +38,47 @@ function ajax_methods(method, addr, data) {
     return response
 }
 
+/**
+ * 
+ * @param {boolean} iserror - True if it is an error; False if not
+ * @param {string} message - Message to display
+ * @param {string} title - Title of the message (Optional)
+ */
+
+function alert(iserror, message, title = "") {
+    $('#alert-message').html(message);
+    $('.alert-toaster').removeClass('d-none');
+    
+    if (title != "")  {
+        $('#alert-title').html(title);
+        $('#alert-title').removeClass('d-none');
+    } else {
+        $('#alert-title').addClass('d-none');
+    }
+
+    if (iserror) {
+        $('.alert-toaster').addClass('error');
+        $('.alert-toaster').removeClass('success');
+    } else {
+        $('.alert-toaster').removeClass('error');
+        $('.alert-toaster').addClass('success');
+    }
+
+
+    $('.alert-toaster').addClass('appear');
+
+    setTimeout(() => {
+        $('.alert-toaster').removeClass('appear');
+        setTimeout(() => {
+            $('.alert-toaster').addClass('d-none');
+        }, 1000);
+    }, 5000);
+}
+
 function reload_page(page_title, content) {
     document.title = "BugMe | " + page_title;
     $('#page-content').html(content);
 }
-
-
-
-
-
 
 });
 
