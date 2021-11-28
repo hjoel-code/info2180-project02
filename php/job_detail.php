5bf885f64b84767c368642cd617a3a7785cbddf7
shae1994 <juddyshaew@gmail.com>
2021-11-28 13:57:37 -0500

    Pulling from the Database functionality achieved.

--- php/job_detail.php
+++ php/job_detail.php
@@ -1,4 +1,4 @@
-<h1>Bug Detail</h1>
+<link rel="stylesheet" href="./styles.css">
 
 <?php 
 
@@ -7,19 +7,44 @@
     
     function getDetails($id){
         $query = "SELECT * FROM `issues` WHERE id = ".$id."" ;
-        
+        $data = array();
         $db = new DatabaseSQL();
         $result = $db->select($query);
         if ($result['state']){
             if ($result['count'] > 0) {
                 // output data of each row
                 while($row = $result['result']->fetch_assoc()) {
-                    echo  $row["id"]." ".$row["title"]. " " . $row["description"]." ".$row["type"]." ".$row["priority"]." ".$row["assigned_to"]." ".$row["created_by"]." ".$row["created"]." ".$row["updated"];
+                    $title = $row['title'];
+                    $id_ = $row['id'];
+                    $description = $row['description'];
+                    $type = $row['type'];
+                    $priority = $row['priority'];
+                    $status = $row['status'];
+                    $assigned_to = $row['assigned_to'];
+                    $created_by = $row['created_by'];
+                    $created = $row['created'];
+                    $updated = $row['updated'];
+                    array_push($data, $title, $id_, $description, $type, $priority, $status, $assigned_to, $created_by, $created, $updated);
+                    return $data;
                 }
             }
+            
         }
+
+        
     }
 
+    
 
-    getDetails($id);
+    $arr = getDetails($id);
+
+    echo "<h1>"."<div id = page-content>".$arr[0]."</h1>";
+
+    echo "<h3>""<div id = page-content>".$arr[1]."</h3>";
+    echo "<p>".$arr[2]."</p>";
+
+    echo "</div>";
+
 ?>
+
+
