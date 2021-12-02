9dea282fa9e82721a171b473d7099cb04b97656d
Ojiugo Okoroafor <amaokoroafor@hotmail.com>
2021-12-01 20:29:27 -0500

    New user form

---
+++ php/js/new_user.js
@@ -0,0 +1,51 @@
+/*window.onload = function (){
+    
+}
+ 
+ 
+ 
+ if (!isValidEmail(email.value.trim())) {
+        validationFailed = true;
+        //alert('Incorrect format for email.');
+        displayErrorMessage(email, "Incorrect format for email address.");
+      };
+*/
+
+/*function validateform(){
+    
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var pass = document.getElementById("pass").value;
+    var email = document.getElementById("email").value;
+    
+
+    function validateFName(){
+        var NameReg = /^[a-zA-Z]+ [a-zA-Z]+$/;
+        var fname = document.getElementById('fname');
+        //if (fname !=null || fname != "" || isNaN(fname)) {
+        if(!NameReg.test(fname)){
+            alert('Please enter a valid first name');
+            document.getElementById('fname').classList.add("invalid");
+            return false;
+        }else{
+            document.getElementById('fname').classList.remove("invalid");
+            return true;
+        }
+    }
+    
+}
+
+
+function validateLName(){
+    var NameReg = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    var lname = document.getElementById('lname');
+}
+
+function validateEmail(){
+    var EmailReg = /^(([^<>()\]\\.,;:\s@";]+(\.[^<>()\[\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+}
+
+function validatePassword(){
+    var PassReg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+}
+*/
--- php/new_user.php
+++ php/new_user.php
@@ -1,1 +1,25 @@
-<h1>New User</h1>
+    <link rel="stylesheet" href="./php/css/new_user.css">
+    <script src="./php/new_user.js"></script>
+    <h1>New User</h1>
+    <form method="post" action="#" onsubmit="return validateform();">
+    
+        <div class = "labels">
+            <label for="firstname">Firstname</label><br/>
+            <input name= "firstname" type="text" id="fname"/><br/> 
+    
+            <label for="lastname">Lastname</label><br/>
+            <input name = "lastname" type="text" id="lname"/><br/>
+    
+            <label for="password">Password</label><br/>
+            <input name = "password" type="password" id="pass"/><br/> 
+
+            <label for="email">Email</label><br/>
+            <input name = "email" type="email" id="email"/><br/>
+
+            <input type="hidden" name="content" value="new_user">
+
+            <input type="submit" id="submit_form" value="Submit" />
+
+        </div>
+    </form>
+
